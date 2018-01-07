d632dcdc96931d527e6e7fc628962f85df28732a
村井健人 <muraikento@muraitaketonoMacBook-puro.local>
2018-01-07 10:59:54 +0900

    新規プロジェクト作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Syuukatu::Application.config.secret_key_base = '852e81cb5f24aeefa8f4b61e955f1ed0b9db258e5836e8eb194320e393389c4026f65342de06431c4de903a1bfb56d1c862010f71c772f4a827c748cd0d7b89b'
