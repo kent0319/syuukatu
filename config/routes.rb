81b4aea0bf8733735d79f58b78e5712504d45eb3
kent0319 <kent0319.purple@gmail.com>
2018-01-14 12:19:19 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Syuukatu::Application.routes.draw do
-  get "static_pages/index"
-  get "static_pages/about"
+  root "static_pages#index"
+  match '/about',  to: "static_pages#about",  via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
